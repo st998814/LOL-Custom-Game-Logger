bd7e7a938c36868b87822ab43c20e00c2c306a4a
st998814 <116810151+st998814@users.noreply.github.com>
2026-01-20 01:36:35 +1030

    refactor of client and session , keep refactor to make main.py as the only entrance

--- client/agent.py
+++ client/agent.py
@@ -13,10 +13,10 @@ Created: 2026-01-18
 """
 
 import aiohttp
-from aiohttp import BasicAuth
+from aiohttp import BasicAuth , ClientSession
 import asyncio
 import time
-from dataclasses import dataclass
+from dataclasses import dataclass ,field
 
 
 from credential_resolver import ProcessInspector , LCUCredential
@@ -28,96 +28,194 @@ from credential_resolver import ProcessInspector , LCUCredential
 # auth_header = BasicAuth("riot" , token)
 
 
-PREFIX = f'https://127.0.0.1:{port}'
-REQUEST_URL = {
-    "GAME_FLOW":f'{PREFIX}/lol-gameflow/v1/gameflow-phase', # game phases 
-    "EOG":f'{PREFIX}/lol-end-of-game/v1/eog-stats-block', 
-    "SESSION":f'{PREFIX}/lol-gameflow/v1/session', # on-going game info 
-    "MATCH" : f'{PREFIX}/lol-match-history/v1/games/' # match info
+# BASE_URL = f'https://127.0.0.1:{port}'
+# REQUEST_URL = {
+#     "GAME_FLOW":f'{BASE_URL}/lol-gameflow/v1/gameflow-phase', # game phases 
+#     "EOG":f'{BASE_URL}/lol-end-of-game/v1/eog-stats-block', 
+#     "SESSION":f'{BASE_URL}/lol-gameflow/v1/session', # on-going game info 
+#     "MATCH" : f'{BASE_URL}/lol-match-history/v1/games/' # match info
 
-}
+# }
 
+# # parse the credentials
+# port , token= LCUCredential(ProcessInspector()).parse()
+
+
+# credentials data place holder
+@dataclass(frozen = True)
+class Credentials:
+    port : int 
+    token : str 
+
+# URLs place holder
 @dataclass
-class Credentials(frozen = True):
-    port : int
-    token : str
+class URLs:
+    credentials: Credentials
+    base: str = field(init=False)
+    suffix: dict = field(default_factory=lambda: {
+        "GAME_FLOW": "lol-gameflow/v1/gameflow-phase",
+        "EOG": "lol-end-of-game/v1/eog-stats-block",
+        "SESSION": "lol-gameflow/v1/session",
+        "MATCH": "lol-match-history/v1/games/"
+    })
+
+    def __post_init__(self):
+        self.base = f"https://127.0.0.1:{self.credentials.port}/"
+
+
+
+    
+class Session:
+
+    def __init__(self , creds : Credentials , urls : URLs):
+        self._cred = creds
+        self._urls = urls
+        self._auth = BasicAuth("riot", creds.token)
+
 
 class Client :
 
     def __init__(self):
         port , token= LCUCredential(ProcessInspector()).parse()
-        self._credential = Credentials(port , token)
-        # self._auth_header = BasicAuth("riot" , token)
+        creds = Credentials(port = port , token = token)
+        urls = URLs(credentials = creds)
+        self._session = Session(creds , urls )
+        
+        
 
-    def _build_auth_header(self,token):
-
-        header = BasicAuth("riot" ,self._credential[token])
-
-        return header
     
+    def _create_session(self)->ClientSession:
+        session =  ClientSession(base_url = self._session._urls.base , auth = self._session._auth )
+
+        return session 
+    
+    def get_suffix(self,api_name:str)->str:
+
+        if api_name in self._session._urls.suffix:
+            suffix = self._session._urls.suffix[api_name]
+            
+        else:
+            raise ValueError (f'Invalid API name:{api_name}')
+
+        return suffix
+
+
+    async def request(self , api_name:str , game_id = None):
+
+        if not game_id:
+            suffix = self.get_suffix(api_name)
+        else:
+            suffix = self.get_suffix(api_name)+f'{game_id}'
+
+        async with self._create_session() as session :
+            async with session.get(suffix, ssl=False) as response:
+                print(response.status)
+                return await response.json()
+
+
+    
+    def __str__(self):
+        pass
+
+
+async def main():
+
+    client = Client()
+    phase = "init"
+
+    while phase != "InProgress":
+                
+        # polling game flow status
+        await asyncio.sleep(1)
+        phase = await client.request("GAME_FLOW")
+        print(phase)
+            
+
+    match_session = await client.request("SESSION")
+    game_id = match_session["gameData"]['gameId']
+
+    while True : 
+        match_data = await client.request("MATCH" , game_id)
+            
+        if "errorCode" in match_data :
+            continue
+        else:
+            break
+
+    print(type(match_data))
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
 
         
 
 
-    def _get_credential(self):
-        """
-        Get port and token 
-        """
-        port , token  = self.credential.parse()
-        return port , token
-
-    
-    def __str__(self):
-
-        port, token = self.get_credential()
-        print(f'Client working on port :{port}')
-
-    
 
 
-
-    
-    async def create_session():
-        pass 
-
-
-# seperated fetches with a single session 
-async def fetch(session, url):
-    async with session.get(url ,ssl = False) as response:
-        print(response.status)
-        print(url)
-        return await response.json()
-
-
-
-async def main():
-    async with aiohttp.ClientSession(auth = auth_header) as session:
-
-
-        while True :
-
-            # checking game phase
-            
-            await asyncio.sleep(1)
-            phase = await fetch(session, REQUEST_URL["GAME_FLOW"])
-            print(phase)
-
-
-            if phase == "InProgress":
-                # start fetching game_id 
                 
-                await asyncio.sleep(1)
-                session_data = await fetch(session , REQUEST_URL["SESSION"])
-                game_id = session_data["gameData"]['gameId']
-                print(game_id)
+                
+            
 
-            # polling till phase == InProgress
-            else:
-                continue
-            match_data = await fetch(session , REQUEST_URL["MATCH"]+f'{game_id}')
-            print(match_data)
+
+
+    
+
+
+
+
+
+asyncio.run(main())
+
+
+
+# # seperated fetches with a single session 
+# async def fetch(session, url:str):
+#     async with session.get(url ,ssl = False) as response:
+#         print(response.status)
+#         print(url)
+#         return await response.json()
+
+
+
+# async def main():
+#     async with aiohttp.ClientSession(auth = auth_header) as session:
+
+
+#         while True :
+
+#             # checking game phase
+            
+#             await asyncio.sleep(1)
+#             phase = await fetch(session, REQUEST_URL["GAME_FLOW"])
+#             print(phase)
+
+
+#             if phase == "InProgress":
+#                 # start fetching game_id 
+                
+#                 await asyncio.sleep(1)
+#                 session_data = await fetch(session , REQUEST_URL["SESSION"])
+#                 game_id = session_data["gameData"]['gameId']
+#                 print(game_id)
+
+#             # polling till phase == InProgress
+#             else:
+#                 continue
+#             match_data = await fetch(session , REQUEST_URL["MATCH"]+f'{game_id}')
+#             print(match_data)
                       
                 
-asyncio.run(main())
+
 
 
