b22a17d4fcf9cdce8ee29a549749859c53033ca0
st998814 <116810151+st998814@users.noreply.github.com>
2026-08-24 01:45:37 +0930

    server : type

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,5 +1,26 @@
-async function registerUser(tgId:string) {
+import type {
+    UserRegister
+
+} from '../types/type.user.js';
+async function registerUser(tgId:string,req:Request):Promise<UserRegister | null> {
     
+    // get user playerID by tgid
+
+        // if  get :
+
+            //  return registrer already
+
+        // if not :
+
+            // TODO: (suppose here we can get user's player id ,
+            // if the requeest body has it)
+
+            // if payer_id present in body 
+                // link(insert) puuid for that row
+            // if not:
+                // complete new user , lodge it's puuid(new row) 
+
 }
+
 
 export {registerUser};
--- server/src/types/type.user.ts
+++ server/src/types/type.user.ts
@@ -1,1 +1,3 @@
 type UserRegister = {}
+
+export type {UserRegister};
